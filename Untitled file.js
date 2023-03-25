c5944f02308ad0c91d6a64abd6a23a5a59ee98a3
J0bej0be222 <jdtibulan@donbosco.edu.ph>
2023-03-25 16:41:10 +0800

    feat: Added new files

---
+++ Untitled file.js
@@ -0,0 +1,16 @@
+import React {Component} from 'react';
+import {View, Test } from 'react-native';
+
+
+class App extends Component {
+  render(){
+    return{
+       <View>
+       <Test>"Hello Word!"</Text>
+      </View>
+    }
+
+  }
+}
+
+export default App;
